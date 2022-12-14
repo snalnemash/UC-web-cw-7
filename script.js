c379b672cbc4cd4a57b5ff160a8b4af0e6d0137d
salnemash <sn.alnemash2@gmail.com>
2022-12-14 20:12:19 +0300

    done

--- script.js
+++ script.js
@@ -5,33 +5,38 @@ let all_grades_div = document.getElementById("all-grades");
 let random_grade_div = document.getElementById("random-grade");
 /* لا تقم بتعديل الأسطر المكتوبة بالأعلى */
 
-
 /*  الجزء الأول والثاني من التمرين */
 
+grades.push(99);
+grades.pop();
+grades.forEach((item) => {
+  all_grades_div.innerHTML += `<span>${item}<span>`;
+});
 
+let randomIndex = Math.floor(Math.random() * grades.length);
 
-
-
-
-
-
+random_grade_div.innerHTML = `<span>#${randomIndex}: ${grades[randomIndex]}<span>`;
 
 /* الجزء الثالث من التمرين */
-
 let course = {
   name: "UniCODE",
-  track : "",
-  studentsCount : 0,
-  location : "Kuwait University"
-}
+  track: "",
+  studentsCount: 0,
+  location: "Kuwait University",
+};
 
 //Change studentsCount,track property here
-
-
-
-//Console log here
-
-
+(studentsCount = 40),
+  (track = "web"),
+  //Console log here
+  Console.log(
+    `Hello my name is sabeha", "i'm in ${course.track} ${course.name}`
+  );
 
 //Create student object here
+let student = {
+  name: sabeha,
+  universty: paaet,
+};
 
+let unicodes = [cat, dog, bird];
